91395e6ce5ec57676361a7f80074987856ab8dfd
Delz <danieloladele92@gmail.com>
2025-08-21 15:08:52 +0100

    Updates

--- src/components/onboarding/Step1AccountSetup.jsx
+++ src/components/onboarding/Step1AccountSetup.jsx
@@ -1,11 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  Box,
-  TextField,
-  CircularProgress,
-  Typography,
-  Alert,
-} from "@mui/material";
+import { Box, TextField, Typography, Select, MenuItem } from "@mui/material";
 
 const Step1AccountSetup = ({ formData, handleChange, setStepValid }) => {
   const [checkingUsername, setCheckingUsername] = useState(false);
